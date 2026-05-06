2db46dff50de815498e3457dfaa8a67f24a94793
Kendawi <elmoizabdulazimkendawi@gmail.com>
2026-05-06 20:28:05 +0300

    foundation: complete exercies 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...rest) {
+    
+    return arr.filter(val => !rest.includes(val));
+
+    /*let newArray = [];
+
+    arr.forEach(item => {
+
+        if (!rest.includes(item)) {
+            newArray.push(item)
+        }
+        
+    });
+
+    return newArray;*/
 };
 
 // Do not edit below this line
